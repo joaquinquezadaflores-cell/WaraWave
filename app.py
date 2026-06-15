2d3aee36039c5837d97ad1bb629b92deb7fdaf58
Joaquin Quezada <joaquin.quezada.flores@alumnos.uta.cl>
2026-06-15 09:15:16 -0400

    Primer paso a la implementación de ingreso de imagenes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from supabase import create_client
+import cv2
+from PIL import Image
+import requests
+import numpy as np
 
 st.title("WaraWave")
 
@@ -14,6 +18,18 @@ try:
     
     st.success("Conexión exitosa con Supabase")
     st.write(respuesta.data)
+
+    st.write("ingrese la imagen de su reporte")
+
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file:
+        image = Image.open(uploaded_file)
+    else:
+        image = Image.open(requests.get("https://picsum.photos/200/120", stream=True).raw)
+
+    edges = cv2.Canny(np.array(image), 100, 200)
+    tab = st.tabs(["Imagen"])
+    tab.image(image, use_column_width=True)
     
 except Exception as e:
     st.error(f"Error: {e}")

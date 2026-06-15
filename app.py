185f85a90d50f0e0f1621bb939b4910e24ec3ebb
Ayleen-cl <ayleenhumire.05@gmail.com>
2026-06-14 23:57:31 -0400

    feat: conexion segura usando streamlit secrets y supabase

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from supabase import create_client
 
-st.title("Hola Ayleen 👋")
-st.write("Mi primera aplicación con Streamlit")
+st.title("WaraWave")
+
+try:
+  
+    url = st.secrets["connections"]["supabase"]["url"]
+    key = st.secrets["connections"]["supabase"]["key"]
+ 
+    supabase = create_client(url, key)
+
+    respuesta = supabase.table("usuarios").select("*").execute()
+    
+    st.success("Conexión exitosa con Supabase")
+    st.write(respuesta.data)
+    
+except Exception as e:
+    st.error(f"Error: {e}")

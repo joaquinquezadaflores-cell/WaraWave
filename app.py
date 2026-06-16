e848723cf914a3007ee76289ec47448b027aa79d
Benjamin-Ag11 <benjaminaguilera1110@gmail.com>
2026-06-16 20:43:04 +0000

    segunda prueba acualizacion de repositorio por spaceCode

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.title("Prueba de actualizacion de repositorio, por spaceCode")
+st.title("Sistema de Gestión - Playa Segura 🏖️")
+
+# Crear una barra lateral para el menú
+opcion = st.sidebar.selectbox("Menú", ["Ver Reportes", "Crear Reporte"])
+
+if opcion == "Crear Reporte":
+    st.subheader("Registrar nuevas condiciones de la playa")
+    playa = st.text_input("Nombre de la playa")
+    estado = st.selectbox("Estado", ["Limpia", "Contaminada", "Flujo Alto"])
+    
+    if st.button("Enviar Reporte"):
+        st.success(f"Reporte de {playa} guardado con éxito.")
